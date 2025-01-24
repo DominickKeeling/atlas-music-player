927f90ea5d192f282487e5b8801ea05334f14e3b
dom <6157@holbertonstudents.com>
2025-01-24 11:39:15 -0600

    inital setup

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,9 @@
+import CoverArtPlaceholder from "../assets/placeholder.svg";
+
+export default function CoverArt() {
+  return (
+    <div className="cover-art">
+      <img src={CoverArtPlaceholder} alt="Cover Art Placeholder" />
+    </div>
+  )
+}
